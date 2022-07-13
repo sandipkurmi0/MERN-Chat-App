33e44ad1a92d376046f61332a601bb814eda9d17
SandipNodejs <sandipkurmi0@gmail.com>
2022-07-13 19:02:22 +0530

    express added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { Route } from 'react-router-dom';
+import Homepaje from './Pages/Homepage'
+import ChatPage from './Pages/ChatPage'
+
+
+function App() {
+  return (
+    <div className="App" >
+      <Route path="/" component={Homepaje} exact />
+      <Route path="/chats" component={ChatPage} />
+    </div>
+  );
+}
+
+export default App;
